9348c64673a0d94beb261bc723aff69d42ef0478
Viniz17 <viniciusdeniz84@gmail.com>
2023-06-21 17:29:21 -0300

    feat(ProfileScreen): adiciona ícones

--- screens/ProfileScreen.tsx
+++ screens/ProfileScreen.tsx
@@ -1,45 +1,47 @@
-import React from 'react';
-import { View, Image, Text, TextInput } from 'react-native';
-import { styles } from '../styles/ProfileScreen'
+import React from "react";
+import { View, Image, Text, TextInput } from "react-native";
+import Icon from "react-native-vector-icons/Feather";
+import { Entypo } from "react-native-vector-icons";
+import { styles } from "../styles/ProfileScreen";
 
 const ProfileScreen = () => {
   return (
     <View style={styles.container}>
-     
-       
-          <Image
-            source={require('../assets/perfil.jpg')}
-            style={styles.avatar}
-          />
-          <Text style={styles.onlineStatus}></Text>
-     
-        <View style={styles.userInfo}>
-          <Text style={styles.name}>Viniz</Text>
-          <Text style={styles.tag}>viniz01</Text>
-          <Text style={styles.tag}>Só não há jeito para a morte.</Text>
-          <View style={styles.separator} />
-          <Text style={styles.text}>NOME EXIBIDO</Text>
-          <TextInput
-          style={styles.input}
-          placeholderTextColor="lightgrey"
-          placeholder="Viniz"
-        />
+      <View style={styles.header}>
+        <Icon name="arrow-left" style={styles.icon} />
+        <Text style={styles.title}>Perfis</Text>
+      </View>
+
+      <View style={styles.headerContainer}>
+        <Image source={require("../assets/perfil.jpg")} style={styles.avatar} />
+        <Text style={styles.onlineStatus}></Text>
+        <Entypo name="edit" style={styles.editIcon} />
+        <Entypo name="edit" style={styles.editIcon2} />
+      </View>
+
+      <View style={styles.userInfo}>
+        <Text style={styles.name}>Viniz</Text>
+        <Text style={styles.tag}>viniz01</Text>
+        <Text style={styles.tag}>Só não há jeito para a morte.</Text>
+        <View style={styles.separator} />
+        <Text style={styles.text}>NOME EXIBIDO</Text>
+        <View>
+          <TextInput style={styles.input} placeholderTextColor="lightgrey" placeholder="Viniz" />
+          <Icon name="x" style={styles.iconInput} />
+        </View>
         <Text style={styles.text}>PRONOMES</Text>
-          <TextInput
+        <TextInput
           style={styles.input}
           placeholderTextColor="lightgrey"
           placeholder="Toque para adicionar pronomes"
         />
-         <Text style={styles.text}>SOBRE MIM</Text>
-          <TextInput
+        <Text style={styles.text}>SOBRE MIM</Text>
+        <TextInput
           style={styles.inputArea}
           placeholderTextColor="lightgrey"
           placeholder="E-mail ou número de telefone"
         />
-       
       </View>
-      
-      
     </View>
   );
 };
--- styles/ProfileScreen.ts
+++ styles/ProfileScreen.ts
@@ -1,102 +1,141 @@
-import { StyleSheet } from "react-native"
+import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#36393E',
-    },
-    header: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      padding: 16,
-    },
-    avatarContainer: {
-      position: 'relative',
-      marginRight: 16,
-    },
-    avatar: {
-      width: 80,
-      height: 80,
-      borderRadius: 40,
-    },
-    onlineStatus: {
-      position: 'absolute',
-      bottom: 4,
-      right: 4,
-      width: 16,
-      height: 16,
-      borderRadius: 8,
-      backgroundColor: 'green',
-    },
-    userInfo: {
-      marginTop: 20,
-      backgroundColor: "#040404",
-      paddingHorizontal: 10,
-      marginHorizontal: 15,
-      borderRadius: 15,
-    },
-    name: {
-      fontSize: 24,
-      fontWeight: 'bold',
-      color: "white",
-      marginTop: 15,
-    },
-    tag: {
-      fontSize: 16,
-      color: 'lightgrey',
-      paddingBottom: 15,
-    },
-    separator: {
-      height: 1,
-      backgroundColor: '#36393E',
-      marginBottom: 15,
-      
-    },
-    content: {
-      padding: 16,
-    },
-    sectionTitle: {
-      fontSize: 20,
-      fontWeight: 'bold',
-      marginBottom: 16,
-    },
-    infoContainer: {
-      flexDirection: 'row',
-      marginBottom: 8,
-    },
-    label: {
-      width: 80,
-      fontWeight: 'bold',
-    },
-    value: {
-      flex: 1,
-    },
-    text: {
-      color: "lightgrey",
-      fontWeight: "bold",
-      marginVertical: 5,
-    },
-    input: {
-      backgroundColor: "#040404",
-      marginVertical: 5,
-      padding: 15,
-      color: "white",
-      borderWidth: 2,
-      borderStyle: "solid",
-      borderColor: "#36393E",
-      borderRadius: 5,
-    },
-    inputArea: {
-      backgroundColor: "#040404",
-      marginVertical: 5,
-      padding: 15,
-      color: "white",
-      borderWidth: 2,
-      borderStyle: "solid",
-      borderColor: "#36393E",
-      borderRadius: 5,
-      height: 160
-    }
-  });
-  
-  
+  container: {
+    flex: 1,
+    backgroundColor: "#36393E",
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 16,
+    marginTop: 10,
+  },
+  headerContainer: {
+    backgroundColor: "#000",
+    height: 160,
+  },
+
+  avatarContainer: {
+    position: "relative",
+    marginRight: 16,
+  },
+  avatar: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    marginTop: 100,
+    marginLeft: 20,
+    border: "solid",
+    borderWidth: 5,
+    borderColor: "#36393E",
+  },
+  onlineStatus: {
+    bottom: 20,
+    left: 75,
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: "lightgreen",
+  },
+  userInfo: {
+    marginTop: 50,
+    backgroundColor: "#040404",
+    paddingHorizontal: 10,
+    marginHorizontal: 15,
+    borderRadius: 15,
+  },
+  name: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+    marginTop: 15,
+  },
+  tag: {
+    fontSize: 16,
+    color: "lightgrey",
+    paddingBottom: 15,
+  },
+  separator: {
+    height: 1,
+    backgroundColor: "#36393E",
+    marginBottom: 15,
+  },
+  content: {
+    padding: 16,
+  },
+  sectionTitle: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginBottom: 16,
+  },
+  infoContainer: {
+    flexDirection: "row",
+    marginBottom: 8,
+  },
+  label: {
+    width: 80,
+    fontWeight: "bold",
+  },
+  value: {
+    flex: 1,
+  },
+  text: {
+    color: "lightgrey",
+    fontWeight: "bold",
+    marginVertical: 5,
+  },
+  input: {
+    backgroundColor: "#040404",
+    marginVertical: 5,
+    padding: 15,
+    color: "white",
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: "#36393E",
+    borderRadius: 5,
+  },
+  inputArea: {
+    backgroundColor: "#040404",
+    marginVertical: 5,
+    padding: 15,
+    color: "white",
+    borderWidth: 2,
+    borderStyle: "solid",
+    borderColor: "#36393E",
+    borderRadius: 5,
+    height: 160,
+  },
+  icon: {
+    top: 20,
+    left: 10,
+    marginBottom: 25,
+    color: "white",
+    fontSize: 24,
+  },
+  title: {
+    color: "white",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginLeft: 50,
+  },
+  iconInput: {
+    color: "black",
+    backgroundColor: "lightgrey",
+    borderRadius: 8,
+    width: 12,
+    height: 12,
+    bottom: 40,
+    left: 300,
+  },
+  editIcon: {
+    color: "white",
+    bottom: 90,
+    left: 80,
+  },
+  editIcon2: {
+    color: "white",
+    bottom: 200,
+    left: 360,
+  },
+});
